9464e204be7dceb32ae19d483a18035ed0acace6
Anatoly <a_gavrik@bk.ru>
2020-12-04 15:04:53 +0300

    sql

--- src/main/java/com/firstspringapplication/dao/CartItemDAO.java
+++ src/main/java/com/firstspringapplication/dao/CartItemDAO.java
@@ -27,10 +27,18 @@ public interface CartItemDAO extends JpaRepository<CartItem, Integer> {
     Integer findTotalSum(Integer userId, Date dateFrom, Date dateTo);
 
     @Query(nativeQuery = true,
-            value = "SELECT SUM(i_price * ci_amount) FROM " +
-                    "(SELECT i.price AS i_price, ci.amount AS ci_amount FROM " +
-                    "cart_items ci " +
-                    "JOIN items i ON i.id = ci.item_id" +
-                    "WHERE ci.cart_id =:cartID)")
+            value = "SELECT SUM(r.it_price * r.it_amount) FROM " +
+                            "(SELECT i.price AS it_price, ci.amount AS it_amount " +
+                            "FROM cart_items ci " +
+                            "JOIN items i ON ci.item_id = i.id" +
+                            "WHERE ci.cart_id = :cartID ) AS r")
     Integer findSumOfCart(Integer cartID);
 }
+
+
+// "SELECT SUM(r.it_price * r.it_amount) FROM " +
+//         "(SELECT ci.amount AS it_amount, i.price AS it_price " +
+//         "FROM cart_items ci " +
+//         "JOIN items i ON ci.item_id = i.id " +
+//         "WHERE ci.cart_id = :cartID)" +
+//         "AS r")
